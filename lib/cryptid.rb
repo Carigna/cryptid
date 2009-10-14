63a484b96a74dc91df1d79558e92d901c63c682a
Toni Leino <toni@leino.net>
2009-10-14 22:22:59 +0300

    First working version with simple int hash

--- lib/cryptid.rb
+++ lib/cryptid.rb
@@ -1,33 +1,51 @@
 module Cryptid
+  require 'crypt/rijndael'
+  PRIME = 2654435761412392
+  MAXID = 2**31-1
+  module ClassMethods
+    def find_with_cryptid( *args )
+        include InstanceMethods
+        options = args.extract_options!
+        validate_find_options(options)
+        set_readonly_option!(options)
 
-  def self.included(cls)
-    cls.extend(ClassMethods)
+        case args.first
+          when :first then find_initial(options)
+          when :last  then find_last(options)
+          when :all   then find_every(options)
+          else             find_from_cryptids(args, options)
+        end
+    end
+
+    def find_from_cryptids(cryptids, options)
+        ids = Array.new
+        cryptids.each { |id|
+            decrypted_id = id.split(/-/)
+	        checksum = (decrypted_id[1].to_i * PRIME & MAXID)
+	        if(checksum == decrypted_id[0].to_i)
+			  ids << decrypted_id[1].to_i
+			end
+        }
+        find_from_ids(ids, options)
+    end
+
+    def to_param
+    end
   end
 
-  module ClassMethods
-
-    def cryptid(key)
-      include InstanceMethods
-      validates_presence_of key
-      self.instance_variable_set(:@cryptid_id, key)
+  def self.included(base)
+    base.class_eval do
+      extend ClassMethods
+      class << self
+        alias_method_chain :find, :cryptid
+      end
     end
-
-    def find_by_cryptid(id)
-      self.send("find_by_id", id)
-    end
-
   end
 
   module InstanceMethods
-
     def to_param
-      send(self.class.instance_variable_get(:@cryptid_id))
+      (id * PRIME & MAXID).to_s + "-" + id.to_s
     end
-
-    def set_param(id)
-      send("#{self.class.instance_variable_get(:@cryptid_id)}=", id)
-    end
-
   end
 
 end
