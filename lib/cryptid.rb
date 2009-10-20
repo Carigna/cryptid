dfb8fd89480eba627b69d96bcf2055e1fa381a41
Toni Leino <toni@leino.net>
2009-10-20 08:46:06 +0300

    Changed checksum to base9

--- lib/cryptid.rb
+++ lib/cryptid.rb
@@ -1,50 +1,44 @@
 module Cryptid
-  PRIME = 2654435761412392
-  MAXID = 2**31-1
+  CRYPTID_PRIME = 2654435761412392
+  CRYPTID_MAXID = 2**31-1
+
   module ClassMethods
-    def find_with_cryptid( *args )
-        include InstanceMethods
-        options = args.extract_options!
-        validate_find_options(options)
-        set_readonly_option!(options)
-
-        case args.first
-          when :first then find_initial(options)
-          when :last  then find_last(options)
-          when :all   then find_every(options)
-          else             find_from_cryptids(args, options)
-        end
-    end
-
-    def find_from_cryptids(cryptids, options)
-        ids = Array.new
-        cryptids.each { |id|
-            decrypted_id = id.to_s.split(/00000/)
-	        checksum = (decrypted_id[1].to_i * PRIME & MAXID)
-	        if(checksum == decrypted_id[0].to_i)
-			  ids << decrypted_id[1].to_i
-			end
-        }
-        find_from_ids(ids, options)
-    end
-
-    def to_param
-    end
-  end
-
-  def self.included(base)
-    base.class_eval do
-      extend ClassMethods
+    def encrypt_id
       class << self
         alias_method_chain :find, :cryptid
       end
+      include InstanceMethods unless included_modules.include?(InstanceMethods)
     end
-    include InstanceMethods unless included_modules.include?(InstanceMethods)
+
+    def find_with_cryptid( *args )
+      options = args.extract_options!
+      validate_find_options(options)
+      set_readonly_option!(options)
+
+      case args.first
+      when :first then find_initial(options)
+      when :last  then find_last(options)
+      when :all   then find_every(options)
+      else             find_from_cryptids(args, options)
+      end
+    end
+
+    def find_from_cryptids(cryptids, options)
+      ids = Array.new
+      cryptids.each do |id|
+        decrypted_id = id.to_s.split(/9/,2)
+        checksum = (decrypted_id[1].to_i * CRYPTID_PRIME & CRYPTID_MAXID)
+        if(checksum == decrypted_id[0].to_i(9))
+          ids << decrypted_id[1].to_i
+        end
+      end
+      find_from_ids(ids, options)
+    end
   end
 
   module InstanceMethods
     def to_param
-      ((id * PRIME & MAXID).to_s + "00000" + id.to_s).to_i
+      ((id * CRYPTID_PRIME & CRYPTID_MAXID).to_s(9) + "9" + id.to_s).to_i
     end
   end
 
